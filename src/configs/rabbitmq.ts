9c5aa6ae6a7aadd65e494bafa3e511db6c0e5917
yusuf <horiyorrmi72@gmail.com>
2025-10-06 12:33:57 +0100

    feat(track):add events[onError and onClose]

--- src/configs/rabbitmq.ts
+++ src/configs/rabbitmq.ts
@@ -4,10 +4,22 @@ const { rabbit } = configVariables;
 
 let connection: amqplib.Connection | null = null;
 
-export async function initQueueConnection(): Promise<void> {
-    if (connection) return;
+export async function initQueueConnection(): Promise<amqplib.Connection> {
+    if (connection) return connection;
     const amqpUrl = rabbit.url
     connection = await amqplib.connect(amqpUrl) as unknown as amqplib.Connection;
+
+    connection.on('error', (err) => {
+        console.error('Rabbit-mq connection Error:', err.message);
+        connection = null;
+    })
+
+    connection.on('close', () => {
+        console.warn('Rabbit-mq connection closed');
+        connection = null;
+    })
+
+    return connection;
 }
 
 export async function checkQueueHealth(): Promise<boolean> {
